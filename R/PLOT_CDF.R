8294365fb045518627ddb1a3aa1cef782ff20e89
rohmerj <j.rohmer@brgm.fr>
2021-09-23 16:54:00 +0200

    Update PLOT_CDF.R

--- R/PLOT_CDF.R
+++ R/PLOT_CDF.R
@@ -1,7 +1,7 @@
 PLOT_CDF <-
 function(Z0,new=TRUE,color1=1,color2=2,...){
 if(new==TRUE){
-dev.new()
+#dev.new()
 plot(ecdf(Z0[1,]),xlim=c(min(knots(ecdf(Z0[1,]))),max(knots(ecdf(Z0[2,])))),col=color1,...)
 lines(ecdf(Z0[2,]),col=color2,...)
 }
