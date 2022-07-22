d26cc867501cdcf1f4a621f2d67e75d3489e9471
rohmerj <j.rohmer@brgm.fr>
2022-07-22 16:53:30 +0200

    Update PROBA_INTERVAL.R
    remove dependence to kerdiest

--- R/PROBA_INTERVAL.R
+++ R/PROBA_INTERVAL.R
@@ -1,8 +1,12 @@
 PROBA_INTERVAL <-
 function(Z0,threshold){
-F1<- kde(type_kernel="e", vec_data=Z0[1,],
-		y=threshold)$Estimated_values
-F2<- kde(type_kernel="e", vec_data=Z0[2,],
-		y=threshold)$Estimated_values
-return(list(Plow=min(F1,F2),Pupp=max(F1,F2)))
+	#F1<- kde(type_kernel="e", vec_data=Z0[1,],
+	#		y=threshold)$Estimated_values
+	#F2<- kde(type_kernel="e", vec_data=Z0[2,],
+	#		y=threshold)$Estimated_values
+	e <- Z0[1,]
+	F1 <- ecdf(e)(threshold)
+	e <- Z0[2,]
+	F2 <- ecdf(e)(threshold)	
+	return(list(Plow=min(F1,F2),Pupp=max(F1,F2)))
 }
